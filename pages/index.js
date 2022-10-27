6ac4ea387dd2d4547146af295a22ee838eff4d1e
Efty <eftyraven@gmail.com>
2022-10-27 21:41:45 +0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useContext } from 'react';
+
+import { Context } from '../context';
+
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const { username, secret, setUsername, setSecret } = useContext(Context);
+
+  const router = useRouter();
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    if (username.length === 0 || secret.length === 0) return;
+    axios
+      .put(
+        'https://api.chatengine.io/users/',
+        { username, secret },
+        { headers: { 'Private-key': 'fa97790f-1c28-4f45-aa5e-dd84404ce682' } }
+      )
+      .then((r) => router.push('/chats'));
+  }
+
+  return (
+    <div className='background'>
+      <div className='auth-container'>
+        <form className='auth-form' onSubmit={(e) => onSubmit(e)}>
+          <div className='auth-title'>Textbot Chat</div>
+          <div className='input-container'>
+            <input
+              placeholder='Email'
+              className='text-input'
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className='input-container'>
+            <input
+              type='password'
+              placeholder='Password'
+              className='text-input'
+              onChange={(e) => setSecret(e.target.value)}
+            />
+          </div>
+          <button type='submit' className='submit-button'>
+            Login/Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
